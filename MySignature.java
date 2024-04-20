8f3d7b2ec51ef11112a56821e9fe012cd139f187
cpaguiar <83176116+cpaguiar@users.noreply.github.com>
2024-04-20 01:00:19 -0300

    Create MySignature.java

---
+++ MySignature.java
@@ -0,0 +1,68 @@
+import java.security.*;
+import java.util.Arrays;
+
+import javax.crypto.*;
+
+
+
+public final class MySignature {
+
+    
+    private static MySignature instance;
+    private static byte[] resumoMensagem;
+    private static String patternAssinatura;
+
+    private static PrivateKey chavePrivada;
+    private static PublicKey chavePublica;
+    private static Cipher cipher;
+    private static MessageDigest providerDigest;
+
+    
+     private MySignature(String pattern) throws Exception {
+        MySignature.patternAssinatura = pattern;
+        MySignature.cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding"); /*"ECIES", "BC"*/
+        MySignature.providerDigest = MessageDigest.getInstance(pattern);
+    }
+
+    public static MySignature getInstance(String pattern) throws Exception {
+        if (instance == null) {
+            instance = new MySignature(pattern);
+        }
+        return instance;
+    }
+
+
+    public static void initSign(PublicKey key) throws Exception {
+        MySignature.chavePublica = key;
+        cipher.init(Cipher.ENCRYPT_MODE, MySignature.chavePublica);
+    }
+
+    public static void update(byte[] textoPlano) throws Exception {
+        MySignature.providerDigest.update(textoPlano);
+        MySignature.resumoMensagem = MySignature.providerDigest.digest();
+    }
+
+    public static byte[] sign() throws Exception {
+        System.out.println("Digest calculated for message:");
+	for(int i = 0; i != MySignature.resumoMensagem.length; i++)
+		System.out.print(String.format("%02X", MySignature.resumoMensagem[i]));
+
+        return MySignature.cipher.doFinal(MySignature.resumoMensagem);
+    }
+
+    public static void initVerify(PrivateKey key) throws Exception{
+        MySignature.chavePrivada = key;
+
+        cipher.init(Cipher.DECRYPT_MODE, MySignature.chavePrivada);
+    }
+
+    public static boolean verify(byte[] assinatura) throws Exception {
+        byte[] DigestAssinatura = MySignature.cipher.doFinal(assinatura);
+        System.out.println("Digest obtained from decrypting digital signature:");
+		for(int i = 0; i != DigestAssinatura.length; i++)
+			System.out.print(String.format("%02X", DigestAssinatura[i]));
+    
+        return Arrays.equals(MySignature.resumoMensagem, DigestAssinatura);
+    }
+
+}
